196bff176a93ff36bfedda70c95f07e642c6cce8
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2020-05-08 20:25:59 +0500

    modify molecule

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,19 +5,10 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-# check if MongoDB is enabled and running
-def test_mongo_running_and_enabled(host):
-    mongo = host.service("mongod")
-    assert mongo.is_running
-    assert mongo.is_enabled
 
-# check if configuration file contains the required line
-def test_config_file(host):
-    config_file = host.file('/etc/mongod.conf')
-    assert config_file.contains('bindIp: 0.0.0.0')
-    assert config_file.is_file
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
 
-# check 27017 port
-def test_mongo_port(host):
-    socket = host.socket('tcp://0.0.0.0:27017')
-    assert socket.is_listening
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
